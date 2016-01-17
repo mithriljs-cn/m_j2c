147a078c3773906171739ae553d29e0c9306633a
mithriljs-cn <mithriljs.cn@gmail.com>
2016-01-17 22:36:04 +0800

    CHANGE: README.md and according m_j2c with some bug fixed

--- README.md
+++ README.md
@@ -1,2 +1,249 @@
 # m_j2c  [![Build Status](https://travis-ci.org/mithriljs-cn/m_j2c.svg?branch=master)](https://travis-ci.org/mithriljs-cn/m_j2c)
-Apply j2c style into mithril
+
+Apply j2c style into DOM & virtual DOM(default support mithril)
+
+## Installation
+
+```Bash
+$ npm install m_j2c
+```
+
+use in browser (can also be used in node)
+
+```JavaScript
+var m_j2c = require('m_j2c')
+```
+
+then below way of apply css to browser DOM:
+
+````Javascript
+var obj= { '.item li':{float:'left'} }	// define style object
+m_j2c( obj, document.querySelector('#some_ul') )	//apply j2c style to #some_ul
+````
+
+This will do 3 things in order:
+
+**1. insert `style` tag into `header`, which contains css:**
+````CSS
+.item_j2c_qh10fm_1imf8sp_8nqwug_810slq_6 li{
+float:left;
+}
+````
+
+**2. cache the generated style name && dom ref**
+
+**3. replace all `.item` class into `.item_j2c_qh10fm_1imf8sp_8nqwug_810slq_6` in `#some_ul` and it's children**
+
+
+
+## Benefit
+
+- **no css name conflict**, all class is localized! ( powered by [j2c](https://github.com/j2css/j2c) )
+- apply **js computation to CSS**!
+- add css **namespaces** to a page/dom, switch using a js call
+
+
+
+## More example
+
+### apply named **style object** to DOM
+````javascript
+var css={
+	navbar: { '.item':{color:'red'} },	// define sytle object
+	content: { '.item':{color:'blue'} }	// define sytle object
+}
+m_j2c.setNS('colorfulSkin')		//will create if not exists
+function applyCSS(){
+	m_j2c.add('navbar', css.navbar)	// add sytle object to `navbar` key in `colorfulSkin` (create if not exists)
+	m_j2c.add('content', css.content)	// add sytle object to `content` key in `colorfulSkin` (create if not exists)
+
+	m_j2c('navbar', document.querySelector('#nav .item') )	// apply style to DOM
+	m_j2c('content', document.querySelector('#content .item') )	// apply style to DOM
+}
+applyCSS()	// now the page style is colorful
+````
+
+above defined a `colorfulSkin` namespace, and two style names `navbar` & `content` in that namespace
+
+then apply to two different dom accordingly.
+
+now `#nav` and `#content` has different style for `.item`, thanks to the `j2c` localized class name.
+
+
+### switch to another namespace
+````javascript
+var css={
+	navbar: { '.item':{color:'white'} },	// define sytle object
+	content: { '.item':{color:'black'} }	// define sytle object
+}
+m_j2c.setNS('blackwhite')		//will create if not exists
+applyCSS()	// now the page style is black'n white
+````
+
+### add / remove some css style from existing rules
+````javascript
+m_j2c.add('navbar', { '.item':{color:'navy', float:'left'} } )  //will update `color`,add `float` for navbar style & dom
+m_j2c.remove('navbar', { '.item':{float:'left'} } )  //will remove `float` for navbar style & dom
+````
+
+
+## Using with [Mithril](http://mithril.js.org)
+
+it's very easy, m_j2c act with a `middleware` for mithril v-dom, so below code:
+````javascript
+var com = {
+	view:function(){
+		return m_j2c('navbar', m('div.item', 'content') )
+	}
+}
+````
+will apply `navbar` in current namespace(e.g. `blackwhite` above) to mithril v-dom, **return new v-dom with class name modified**
+
+and later if you want to change style, just:
+````javascript
+m_j2c.add('navbar', { '.item':{color:'navy', float:'left'} } )  // will trigger m.redraw, and update the v-dom to apply new style
+````
+
+## `m_j2c` API
+
+### *Variables*
+
+#### m_j2c.DEFAULT_NS
+- *string* internal var store, initial value is 'default', you can change directly, when first used
+
+#### m_j2c.j2c
+- *function* j2c reference, so you don't need `require('j2c')`. Mostly, you can use m_j2c to manager your j2c
+
+
+
+### *Functions*
+
+### m_j2c.setNS(ns)
+**Set current namespace to ns, and refresh dom; previously applied dom style of current namespace will be reverted **
+- `ns` [*string* *optional*] namespace. if omit, use `m_j2c.DEFAULT_NS`
+````
+m_j2c.setNS()	//switch to default namespace
+m_j2c.setNS('colorful')	//set colorful as current namespace, and switch to it
+````
+
+
+### m_j2c.getNS()
+**Get current active namespace **
+````
+m_j2c.getNS()	//'colorful'
+````
+
+
+### m_j2c.removeNS(ns)
+**Remove namespace and it's style, swtich to default namespace(m_j2c.DEFAULT_NS) **
+- `ns` [*string*] namespace to remove
+````
+m_j2c.removeNS('colorful')	//remove colorful name space, and switch current namespace to 'default'
+````
+
+
+### m_j2c(m)
+**Specify mithril instance, for trigger m.redraw**
+- `m` [*function*] mithril `m` instance. will check `window.m` internally first, or specified use it. `add`, `remove`, `setNS`, `removeNS` function will trigger a `m.redraw`. If no `m` detected, m_j2c also work except auto redraw. Only need call once.
+- **return** `m_j2c` handler to allow chain with other function
+````
+m_j2c(m).add('name', cssObj)	//set m and
+````
+
+
+### m_j2c(cssObj, [dom||v-dom])
+**Apply cssObj rule to dom/v-dom, using an auto generated name**
+- `cssObj` [*object*] style to apply, auto generate a temp name
+- `dom||v-dom` [*object* *optional*] the target to apply style.
+- **return** modified `v-dom` or `dom` list that applied j2c class
+````
+m_j2c( {'.item':{ color:'red' }} , document.querySelector('div.item') )	//apply cssObj to dom `div.item` and it's children
+m_j2c( {'.item':{ color:'red' }} , m('.item', 'content') )	// apply cssObj to  mithril `m` result
+````
+
+
+### m_j2c([ns], name, [dom||v-dom])
+**Apply class rule to dom/v-dom**
+- `ns` [*string* *optional*] namespace. if omit, use current namespace; `''` denote `m_j2c.DEFAULT_NS`
+- `name` [*string*] name of ns to get style sheet, the `ns` and `name` just key path internally
+- `dom||v-dom` [*object* *optional*] the target to apply style.
+  For `dom`, just an alias for `m_j2c.applyClass` function
+  For `v-dom`, transform to apply j2c. anything that is not normal `v-dom` will **return as is**, like `m.component`, `{subtree:retain}`
+- **return** modified `v-dom` or `dom` list that applied j2c class
+````
+m_j2c('name', document.querySelector('div.abc') )	//apply current namespace:name style to dom `div.abc` and it's children
+m_j2c('name', m('.item', 'content') )	// apply current namespace:name style to  mithril `m` result
+m_j2c('', 'name', m('.item', 'content') )	//apply default namespace:name style to mithril `m` result
+````
+
+
+### m_j2c.add([ns], name, cssObj)
+**Add class rule to ns:name, extend from cssObj and refresh dom/v-dom**
+- `ns` [*string* *optional*] namespace. if omit, use current namespace; `''` denote `m_j2c.DEFAULT_NS`
+- `name` [*string*] name of ns to get style sheet, the `ns` and `name` just key path internally
+- `cssObj` [*object* *optional*] the style object to be added
+````
+m_j2c.add('name', {'.item':{ color:'red' }} )	//add red color to .item in current namespace:name, and update style&dom
+m_j2c.add('', 'name', {'.item':{ color:'red' }} )	//add red color to .item in default namespace:name, and update style&dom
+````
+
+
+### m_j2c.remove([ns], name, [cssObj])
+**Remove class rule from ns:name, which has truthy value in cssOb and refresh dom/v-domj**
+- `ns` [*string* *optional*] namespace. if omit, use current namespace; `''` denote `m_j2c.DEFAULT_NS`
+- `name` [*string*] name of ns to get style sheet, the `ns` and `name` just key path internally
+- `cssObj` [*object* *optional*] the style object to be removed, which has truthy value in cssObj. If falsy, will detele ns:name style totally
+````
+m_j2c.remove('name' )	//remove current namespace:name totally
+m_j2c.remove('name', {'.item':{ color:'red' }} )	//remove red color to .item in current namespace:name, and update style&dom
+m_j2c.remove('', 'name', {'.item':{ color:'red' }} )	//remove red color to .item in default namespace:name, and update style&dom
+````
+
+
+### m_j2c.applyClass([ns], name, [target])
+**Apply DOM node class name into j2c, return affected dom list**
+- `ns` [*string* *optional*] namespace. if omit, use current namespace; `''` denote `m_j2c.DEFAULT_NS`
+- `name` [*string*] name of ns to get style sheet, the `ns` and `name` just key path internally
+- `target` [*DOM* *optional*] target to apply style to. If falsy, `document.body` will be used
+````
+m_j2c.applyClass('name' )	//apply current namespace:name to body and it's children
+m_j2c.applyClass('name', document.querySelector('#some_ul') )	//apply current namespace:name to #some_ul and it's children
+m_j2c.applyClass('', 'name', document.querySelector('#some_ul') )	//apply default namespace:name to #some_ul and it's children
+````
+
+
+### m_j2c.revertClass([target], [ns], [name])
+**Revert DOM node class name into original, return affected dom list**
+- `target` [*DOM* *optional*] target to apply style to. If falsy, `document.body` will be used
+- `ns` [*string* *optional*] namespace. if omit, use current namespace; `''` denote `m_j2c.DEFAULT_NS`
+- `name` [*string* *optional*] name of ns to get style sheet, the `ns` and `name` just key path internally
+````
+m_j2c.revertClass()	//revert all style of current namespace in body and it's children
+m_j2c.revertClass(document.querySelector('#some_ul') )	 //revert all style of current namespace in #some_ul and it's children
+m_j2c.revertClass(document.querySelector('#some_ul'), null, 'name')	 //revert style of current namespace:name in #some_ul and it's children
+m_j2c.revertClass(document.querySelector('#some_ul'), '', 'name' )	//revert style of default namespace:name to #some_ul and it's children
+````
+
+
+### m_j2c.getClass([ns], name)
+**Get class-j2c class map for name, return `object`, key is original class name, value is j2c class name **
+- `ns` [*string* *optional*] namespace. if omit, use current namespace; `''` denote `m_j2c.DEFAULT_NS`
+- `name` [*string*] name of ns to get style sheet, the `ns` and `name` just key path internally
+````
+m_j2c.getClass('name' )	//get current namespace:name style map
+m_j2c.getClass('', 'name' )	//get default namespace:name style map
+````
+
+
+### m_j2c.domClassMap()
+**Return internal dom list array which has j2c class applied **
+
+
+
+
+
+
+
+
+
+
--- src/m_j2c.js
+++ src/m_j2c.js
@@ -1,28 +1,26 @@
-var j2c = require('j2c')
-var util = require('util_extend_exclude')
-// var m = window.m || require('mithril')
-var m = function(){}	// user should pass m to this module, for optimize file size
+import j2c from 'j2c'
+import util from 'util_extend_exclude'
+// import m from 'mithril'
+// have to manually set m ref when required
+var m= typeof window!='undefined'&&window.m||null
 
-var DEFAULT_NS = 'global_j2c'
-var namespace = DEFAULT_NS
+var OBJECT = "[object Object]", ARRAY = "[object Array]", STRING = "[object String]", REGEXP="[object RegExp]";
+var type = {}.toString;
+
 var j2cGlobal = {}
-var j2cStore = {}
 var domClassMap = []
 
-j2cGlobal[namespace] = j2cStore;
+m_j2c.DEFAULT_NS = 'default';
+m_j2c.j2c = j2c;
+var namespace = m_j2c.DEFAULT_NS
+
+j2cGlobal[m_j2c.DEFAULT_NS] = {};
 
 var isBrowser = typeof document==='object' && document && document instanceof Node;
 
-function findDom(dom) {
-	for(var i=0, n=domClassMap.length; i<n; i++ ){
-		if(domClassMap[i].dom === dom) return i
-	}
-	return -1
-}
-
 // check if the given object is HTML element
-function isElement(o){return (typeof HTMLElement === "object" ? o instanceof HTMLElement :o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"); }
-
+function isElement(o){return (typeof HTMLElement == "object" ? o instanceof HTMLElement :o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"); }
+function isComponent(o){return typeof o=='object'&&o&&typeof o.view=='function' }
 function stylize(element, sheet){
 	if(!isBrowser) return;
     element.type = 'text/css';
@@ -36,14 +34,17 @@ function stylize(element, sheet){
     return element;
 }
 
-function addStyleToHead(styleObj) {
+function addStyleToHead(styleObj, ns, name) {
 	if(!isBrowser) return;
+	name=name||''
+	var id = 'style_'+ ns + '_' + name + '_' + styleObj.version
+	if( document.getElementById(id) ) return;
 	if(!styleObj.dom){
 		var el = document.createElement('style')
 		document.head.appendChild(el)
 		styleObj.dom = el
 	}
-	styleObj.dom.setAttribute('data-ns', namespace + '_' + styleObj.version)
+	styleObj.dom.setAttribute('id', id.replace(/\"/g,'&quot;').replace(/\'/g,'&apos;') )
 	stylize(styleObj.dom, styleObj.sheet)
 }
 
@@ -56,12 +57,13 @@ function intervdom (sheet, vdom){
 			return c
 		}).join(' ')
 	}
-	if(vdom.children) vdom.children.forEach(function(v){ applyStyle(sheet, v)  } )
+	if( type.call(vdom.children) ===ARRAY ) vdom.children.forEach(function(v){ applyStyle(sheet, v)  } )
 	return vdom
 }
 function applyStyle (sheet, vdom){
-	if( {}.toString.call(vdom)==="[object Array]" ) return vdom.map( function(v){ return applyStyle(sheet, v) } );
-	return [intervdom(sheet, vdom)]
+	if( type.call(vdom)===ARRAY ) return vdom.map( function(v){ return applyStyle(sheet, v) } );
+	if( type.call(vdom)!==OBJECT) return vdom
+	return intervdom(sheet, vdom)
 }
 
 function removeDom (styleObj) {
@@ -71,73 +73,90 @@ function removeDom (styleObj) {
 	delete styleObj.dom
 }
 
-function m_j2c(name, vdom) {
-	// usage: m_j2c() will return all j2cStore
-	if(!name) return j2cStore;
-	if( isElement(name) ) return m_j2c.applyDom.apply(this, arguments);
-	var styleObj = j2cStore[name]
-	// usage: m_j2c('name') will return all j2cStore['name']
-	if(!vdom) return styleObj;
+function m_j2c(ns, name, vdom) {
+	var args = arguments;
+	if(args.length===0) return j2cGlobal;	//m_j2c()
+	if(args.length===1){
+		//if it's mithril ref
+		if(typeof ns=='function' && ns.prop){	//m_j2c(m)
+			m=ns; return m_j2c;
+		}
+		name=ns, ns=namespace;	//m_j2c('name')-> curNS,name
+	}
+	if(typeof args[1]=='object') vdom=name, name=ns, ns=namespace;	//m_j2c('name', vdom)-> curNS,name(vdom)
+	ns=ns||m_j2c.DEFAULT_NS	//m_j2c('', 'name') -> DEFAULT_NS, name
+
+	if(!name) return j2cGlobal[ns];	//m_j2c('')->curNS  ||  m_j2c('', '')->DEFAULT_NS
+	else if(!vdom) return j2cGlobal[ns]&&j2cGlobal[ns][name];	//m_j2c('name')->curNS,name &&  m_j2c('', 'name')->DEFAULT_NS,name
+
+	if( type.call(name)==OBJECT ){
+		//support {'.item li':{float:left}} as name
+		var tempName='temp_'+Date.now()+Math.random()
+		m_j2c.add(ns, tempName, name)
+		name=tempName
+	}
+	if( isElement(vdom) ) return m_j2c.applyClass(ns, name, vdom);
+	var styleObj = j2cGlobal[ns][name]
 	// usage: m_j2c('name', mithril_v_dom) will add style to vdom and create <style> for it, cache style_dom
 	if( !styleObj || !styleObj.sheet ) return applyStyle({}, vdom);
-	var sheet = styleObj.sheet;
-	var styleDom = m('style', {
-		config:function(el, isinit, context, vdom){
-			if(!isinit) {
-                stylize(el, sheet);
-                styleObj.dom = el;
-            }
-		}
-	});
-	styleDom.attrs[ 'data-'+ namespace + '_' + name+'_'+styleObj.version ] = true;
 	// Known Issue: the dom will always re-created when pass to mithril, so we set below to skip next redraw()
-	m.redraw.strategy('none')
-	return [ styleDom, applyStyle(sheet, vdom) ]
+	// m.redraw.strategy('none')
+	var ret = applyStyle(styleObj.sheet, vdom)
+	// console.log(ret)
+	return  ret
 }
-
-m_j2c.m = m;
-m_j2c.j2c = j2c;
 m_j2c.removeNS = function( ns ){
-	if(!ns) return;
+	// DEFAULT_NS cannot be removed
+	if(!ns||ns===m_j2c.DEFAULT_NS) return;
 	if(ns===namespace){
+		var j2cStore = j2cGlobal[ns]
 		for(var i in j2cStore){
 			removeDom( j2cStore[i] )
 		}
-		m_j2c.applyClass(null,null,false)
-		namespace = DEFAULT_NS
-		j2cStore = j2cGlobal[namespace]
-		m.redraw()
+		m_j2c.revertClass()
+		namespace = m_j2c.DEFAULT_NS
+		m&&m.redraw()
 	}
-	return delete j2cGlobal[ns]
+	delete j2cGlobal[ns]
+	return m_j2c
 }
 m_j2c.getNS = function( ns ){
 	return ns ? j2cGlobal[ns] : namespace
 }
 m_j2c.setNS = function( ns ){
-	if(ns){
-		namespace = ns;
-		for(var i in j2cStore){
-			removeDom( j2cStore[i] )
-		}
-		m_j2c.applyClass(null,null,false)
-		if(!j2cGlobal[namespace]) j2cGlobal[namespace] = j2cStore = {} ;
-		else j2cStore = j2cGlobal[namespace];
-		for(i in j2cStore){
-			m_j2c.add( i, j2cStore[i].cssObj )
-		}
-		m.redraw()
+	var j2cStore = j2cGlobal[namespace]
+	ns = ns||m_j2c.DEFAULT_NS
+	namespace = ns;
+	for(var i in j2cStore){
+		removeDom( j2cStore[i] )
 	}
+	m_j2c.revertClass()
+
+	if(!j2cGlobal[namespace]) j2cGlobal[namespace] = j2cStore = {} ;
+	else j2cStore = j2cGlobal[namespace];
+	for(i in j2cStore){
+		m_j2c.add( i, j2cStore[i].cssObj )
+	}
+	m&&m.redraw()
+
 	return m_j2c
 }
-m_j2c.setM = function(mithrilGlobal) {
-	if(mithrilGlobal) m = mithrilGlobal;
-	return m_j2c;
-}
+m_j2c.add = function( ns, name, cssObj ) {
+	var args = arguments;
+	if(args.length===0) return j2cGlobal[namespace];
+	if(args.length===1) name=ns, ns=namespace;
+	if(typeof args[1]=='object') cssObj=name, name=ns, ns=namespace;
+	ns=ns||m_j2c.DEFAULT_NS
 
-m_j2c.add = function( name, cssObj ) {
-	if(!name)return;
+	if(!name)return j2cGlobal[ns];
+	else if(!cssObj)return j2cGlobal[ns]&&j2cGlobal[ns][name];
+
+	var j2cStore = j2cGlobal[ns]
+	if(!j2cStore) j2cGlobal[ns]={};
+	// revert all class for ns/name
+	var changeList=[]
+	domClassMap.forEach(v=>_addClassToDom(v.dom,changeList,ns,name,false))
 	var styleObj
-	var isHead = name.indexOf('<head')===0;
 	if(!j2cStore[name]){
 		styleObj = j2cStore[name] = { cssObj:cssObj, version:0, sheet:j2c.sheet(cssObj) };
 	} else {
@@ -146,77 +165,123 @@ m_j2c.add = function( name, cssObj ) {
 		styleObj.sheet = j2c.sheet(styleObj.cssObj);
 		styleObj.version++
 	}
-	if( isHead ) addStyleToHead(styleObj)
-	else if( styleObj.dom ) m.redraw();
-
+	addStyleToHead(styleObj, ns, name)
+	m&&m.redraw();
+	changeList.forEach(v=>_addClassToDom(v.dom,[],ns,name,true))
 	return j2cStore[name];
 }
-m_j2c.remove = function(name, cssObj) {
-	if(!name)return;
-	var isHead = name.indexOf('<head')===0;
+m_j2c.remove = function(ns, name, cssObj) {
+	var args = arguments;
+	if(args.length===0) return j2cGlobal[namespace];
+	if(args.length===1) name=ns, ns=namespace;
+	if(typeof args[1]=='object') cssObj=name, name=ns, ns=namespace;
+	ns=ns||m_j2c.DEFAULT_NS
+
+	var j2cStore = j2cGlobal[ns]
+	if(!name || !j2cStore || !j2cStore[name]) return;
+	// revert all class for ns/name
+	var changeList=[]
+	domClassMap.forEach(v=>{
+		_addClassToDom(v.dom,changeList,ns,name,false)
+	})
 	var styleObj = j2cStore[name];
 	if(!cssObj){
 		delete j2cStore[name]
+		removeDom( styleObj )
 	}else{
 		util._exclude(styleObj.cssObj, cssObj, null);
 		styleObj.sheet = j2c.sheet(styleObj.cssObj);
 		styleObj.version++
+		addStyleToHead(styleObj, ns, name)
+		changeList.forEach(v=>_addClassToDom(v.dom,[],ns,name,true))
 	}
-	if( isHead ) {
-		cssObj
-		? addStyleToHead(styleObj)
-		: removeDom( styleObj )
-	}
-	else if( styleObj.dom ) m.redraw();
-
+	m&&m.redraw();
 	return styleObj
 }
-m_j2c.getClass = function (nameRegex){
-	var sheet, list = {}
-	nameRegex = nameRegex||/./
-	for(var i in j2cStore){
+m_j2c.getClass = function (ns, name) {
+	var args = arguments
+	if(args.length===0) return j2cGlobal[namespace];	//getClass()->curNS
+	if(args.length===1) name=ns, ns=namespace	//getClass('name')->curNS,name
+	ns=ns||m_j2c.DEFAULT_NS	//getClass('', 'name')->DEFAULT_NS,name
+	var sheet, list = {}, store= j2cGlobal[ns]
+	if(!store||!name) return;
+	for(var i in store){
 		// tutpoint: string.match(undefined) ?
-		if( (sheet=j2cStore[i].sheet) && i.match(nameRegex) ){
-			for(var name in sheet){ if(sheet.hasOwnProperty(name)&& !name.match(/^\d/) ) list[name]=sheet[name] }
+		if( (sheet=store[i].sheet) && ( type.call(name)==REGEXP ? i.match(name) : i==name ) ){
+			for(var key in sheet){ if(sheet.hasOwnProperty(key)&& !key.match(/^\d/) ) list[key]=sheet[key] }
 		}
 	}
-	// console.log(list)
 	return list;
 }
-m_j2c.getClassMap = function () {
+
+var _addClassToDom = function(dom, domRange, ns, name, isAdd) {
+	var pos, c = dom.className&&dom.className.split(/\s+/)
+    if(c) dom.className = c.map(function(v){
+    	if( isAdd===false ) {
+    		for(var i=domClassMap.length;i--;){
+    			var d=domClassMap[i]
+    			ns=ns||d.ns
+    			name=name||d.name
+    			if(d.ns==ns&&d.name==name&&d.dom===dom && d.j2c==v){
+    				domRange&&domRange.push( d )
+    				return d.original
+    			}
+    		}
+    		return v
+    	}else{
+    		var list = m_j2c.getClass(ns, name)
+    		if(!list)return
+	    	var j2cClass = list[v]
+	    	if(j2cClass){
+	    		var obj={ ns:ns, name:name, dom:dom, original:v, j2c:j2cClass }
+	    		domRange&&domRange.push( obj );
+	    		domClassMap.push( obj );
+	    	}
+	    	return j2cClass||v
+    	}
+    }).join(' ')
+}
+
+m_j2c.domClassMap = function (){
 	return domClassMap
 }
-m_j2c.applyClass = function (target, nameRegex, isClear){
-	if(! isBrowser) return;
-	var list = m_j2c.getClass(nameRegex)
-	var _addClassToDom = function(dom){
-		var pos, c = dom.className&&dom.className.split(/\s+/)
-	    if(c) dom.className = c.map(function(v){
-	    	if( isClear===false ) {
-	    		if((pos=findDom(dom))!==-1){
-		    		var old = domClassMap[pos].original
-		    		domClassMap.splice(pos,1)
-		    		return old
-	    		}else{
-	    			return v
-	    		}
-	    	}else{
-		    	var j2cClass = list[v]
-		    	if(j2cClass) domClassMap.push( { dom:dom, original:v } );
-		    	return j2cClass||v
-	    	}
-	    }).join(' ')
-	}
-	if( !isElement(target) ) target=document.body;
-	_addClassToDom(target)
+m_j2c.revertClass = function (target, ns, name) {
+	var domRange=[]
+	if( !target||!isElement(target) )target=document.body;
+	if(ns==='') ns=m_j2c.DEFAULT_NS;
+	ns=ns||namespace
+	_addClassToDom(target,domRange,ns,name,false)
 	var items = target.getElementsByTagName("*")
 	for (var i = items.length; i--;) {
-	    _addClassToDom(items[i])
+	    _addClassToDom(items[i],domRange,ns,name,false)
 	}
+	return domRange
+}
+m_j2c.applyClass = function (ns, name, target){
+	if(! isBrowser) return;
+	var args = arguments;
+	if(typeof args[1]=='object') target=name, name=ns, ns=namespace;
+	if(name===undefined) name=ns, ns=namespace;
+	if( !target||!isElement(target) )target=document.body;
+	ns=ns||m_j2c.DEFAULT_NS
+	if( type.call(name)==OBJECT ){
+		//support {'.item li':{float:left}} as name
+		var tempName='temp_'+Date.now()+Math.random()
+		m_j2c.add(ns, tempName, name)
+		name=tempName
+	}
+	var domRange=[]
+	_addClassToDom(target, domRange, ns, name)
+	var items = target.getElementsByTagName("*")
+	for (var i = items.length; i--;) {
+	    _addClassToDom(items[i], domRange, ns, name)
+	}
+	return domRange
 }
 
+export default m_j2c
 
-exports = module.exports = m_j2c;
+// exports = module.exports = m_j2c;
 
 // Usage:
 // m_j2c.add( '<head abc>', {' body':{font_size:'10px', }} )
